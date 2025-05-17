fee7223a7d8b915c85174c2578e3a09d7f1f1767
QodirovDeveloper <startfly58@gmail.com>
2025-05-17 20:33:55 +0500

    add

--- app.js
+++ app.js
@@ -135,3 +135,49 @@ function Print(users) {
 }
 
 Print(users); // 🔄 Ma'lumotni chiqarish
+
+// !===========================================
+// ! Login
+
+const form = document.getElementById("userrForm");
+const userrList = document.getElementById("userrList");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+
+nameInput.value = localStorage.getItem("inputName") || "";
+emailInput.value = localStorage.getItem("inputEmail") || "";
+nameInput.addEventListener("input", () => {
+  localStorage.setItem("inputName", nameInput.value);
+});
+
+emailInput.addEventListener("input", () => {
+  localStorage.setItem("inputEmail", emailInput.value);
+});
+
+let userss = JSON.parse(localStorage.getItem("userss")) || [];
+function renderUsers() {
+  userrList.innerHTML = "";
+  userss.forEach((userr) => {
+    const li = document.createElement("li");
+    li.textContent = `${userr.name} - ${userr.email}`;
+    userrList.appendChild(li);
+  });
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+
+  if (name && email) {
+    const userr = { name, email };
+    userss.push(userr);
+    localStorage.setItem("userss", JSON.stringify(userss));
+    renderUsers();
+    form.reset();
+    localStorage.removeItem("inputName");
+    localStorage.removeItem("inputEmail");
+  }
+});
+
+renderUsers();
